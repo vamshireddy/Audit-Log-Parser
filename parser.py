2d22b1c66ae18e5fa46d8d8e460673e202b32f25
Vamshi <k.vamshi2008@gmail.com>
2015-07-06 00:13:58 +0530

    Parsing the logs and inserting to mongo

--- parser.py
+++ parser.py
@@ -1,24 +1,80 @@
 # This program will generate the log files, parse the log files and store them on the mongoDB.
 
-import sys
+import os
+import time
+import re
+import json
+import pymongo
+from pymongo import MongoClient
 
 output_events_file = "events.log"
 output_stats_file = "stats_json.log"
+unified_log_file_name = "newnslog.ppe.0"
+offset = 0
+db_name = "auditlog-db"
+col_name = "col"
 
-def generateAuditLog(unified_log_file_name):
+def generateAuditLog():
 	""" This function invokes the command nsconmsg to generate the events in LOG format"""
-	system("./nsconmsg -K "+unified_log_file_name+" -d auditlog >"+output_events_file)
+	os.system("./nsconmsg -K "+unified_log_file_name+" -d auditlog >"+output_events_file)
 
-def generateStatsJSON(unified_log_file_name):
+def generateStatsJSON():
 	""" This function invokes the command nsconmsg to generate the Stats in JSON format"""
-	system("./nsconmsg -K "+unified_log_file_name+" -d auditlog >"+output_stats_file)
+	os.system("./nsconmsg -K "+unified_log_file_name+" -d auditlog >"+output_stats_file)
 
-
-
-def parseAuditLogToJSON():
-	""" Regex: (\d+\/\d+\/\d+:\d+:\d+:\d+)\s+(\S+)?\s+(\S+)?\s+(\S+)\s+(\S+)\s+:\s+([a-zA-Z0-9_ ]+)\s+([a-zA-Z0-9_ ]+)\s+(\d+)\s+(\d+)\s+:\s+(.+) """
+def parseAuditLogToJSON(col):
+	""" 
+	Regex:(\d+\/\d+\/\d+:\d+:\d+:\d+)\s+(\S+)?\s+(\S+)?\s+(\S+)\s+(\S+)\s+:\s+([a-zA-Z0-9_ ]+)\s+([a-zA-Z0-9_ ]+)\s+(\d+)\s+(\d+)\s+:\s+(.+) 	"""
 	# Read the audit log file line by line into buffer, match the re with the current line
-	
-	match = re.search( r'(\d+\/\d+\/\d+:\d+:\d+:\d+)\s+(\S+)?\s+(\S+)?\s+(\S+)\s+(\S+)\s+:\s+([a-zA-Z0-9_ ]+)\s+([a-zA-Z0-9_ ]+)\s+(\d+)\s+(\d+)\s+:\s+(.+)', buff, re.M|re.I)
-        if match == None:
-		return None
+	file_obj = open(output_events_file, "r")
+	global offset
+	if file_obj == None :
+		print "File can't be opened"
+		sys.exit(243)
+	file_obj.seek(offset, 0)
+	while True:
+		line = file_obj.readline()
+		print line
+		if line == "" :
+			print "Done"
+			offset = file_obj.tell()
+			break
+		time.sleep(0.1)
+		match = re.search( r'(\d+\/\d+\/\d+:\d+:\d+:\d+)\s+(\S+)?\s+(\S+)?\s+(\S+)\s+(\S+)\s+:\s+([a-zA-Z0-9_ ]+)\s+([a-zA-Z0-9_ ]+)\s+(\d+)\s+(\d+)\s+:\s+(.+)', line, re.M|re.I)
+		if match == None:
+			print "Not matched"
+		else:
+			print "Matched!"
+			data = {}
+			data['time'] = match.group(1)
+			data['time_std'] = match.group(2)
+			data['log_format'] = match.group(3)
+			data['type'] = match.group(4)
+			data['ppe'] = match.group(5)
+			data['event_type'] = match.group(6)
+			data['event_subtype'] = match.group(7)
+			data['event_no'] = match.group(8)
+			data['event_subno'] = match.group(9)
+			data['desc'] = match.group(10)
+			print data
+			insertToDB(data,col)
+			print "\n"
+	file_obj.close()
+
+
+def insertToDB(json, col):
+	""" Insert the json into database """
+	col.insert_one(json)
+
+# Setup pymongo
+client = MongoClient()
+db = client[db_name]
+collection = db[col_name]
+
+while True:
+	# Create log files
+	generateAuditLog()
+	# generateStatsJSON()
+	# Parse the log files
+	parseAuditLogToJSON(collection)
+	time.sleep(60)
